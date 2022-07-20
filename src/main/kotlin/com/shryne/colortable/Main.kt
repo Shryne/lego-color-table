da52fa77361f78ca721eff11755b32ec1b4ec165
Shryne <enomeral@gmx.de>
2022-07-20 22:20:22 +0200

    print all colors

--- src/main/kotlin/com/shryne/colortable/Main.kt
+++ src/main/kotlin/com/shryne/colortable/Main.kt
@@ -1,5 +1,23 @@
 package com.shryne.colortable
 
+import kong.unirest.Unirest
+import org.jsoup.Jsoup
+
 fun main() {
-    println("HEY")
+    //println(Unirest.get()
+    //    .asString().body)
+
+    val html = Jsoup.connect("https://www.bricklink.com/catalogColors.asp?sortBy=N")
+        .get()
+
+    val table = html.body().getElementById("id-main-legacy-table")
+    val rows = table!!.getElementsByTag("table")[3].getElementsByTag("table").get(1).getElementsByTag("tr")
+
+    rows.forEach {
+        val name = it.getElementsByTag("td")[3].getElementsByTag("font").text()
+        //val id = it.getElementsByTag("td")[0].getElementsByTag("font")
+        //val sale = it.getElementsByTag("td")[7].getElementsByTag("font")
+        //println("name: $name, id: $id, for sale: $sale" )
+        println(name)
+    }
 }
